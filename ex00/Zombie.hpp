cf94f48d785839197cae7e6cf5703a57907011bb
Martin LOUVARD <malouvar@e1r9p19.42.fr>
2022-03-17 12:17:34 +0100

    first commit

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,17 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+class Zombie{
+
+    public:
+    //Constructors seters and geters
+    void announce( void );
+
+    private:
+    //Attributes
+    std::string name;
+};
+#endif
