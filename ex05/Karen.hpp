f7852a12c634d3176dfabf8e2607fda9116c6b2d
Martin <martin.louvard@gmail.com>
2022-03-21 16:01:35 +0100

    ex05

---
+++ ex05/Karen.hpp
@@ -0,0 +1,23 @@
+#ifndef KAREN_HPP
+#define KAREN_HPP
+
+#include <string>
+
+class Karen{
+    public:
+
+    void complain( std::string level );
+
+    private:
+
+    void debug( void );
+    void info( void );
+    void warning( void );
+    void error( void );
+
+};
+
+typedef struct s_type{
+void (Karen::*f)(void);
+} t_type;
+#endif
---
+++ ex05/main.cpp
@@ -0,0 +1,14 @@
+#include "Karen.hpp"
+
+int main()
+{
+	Karen	karen;
+
+	karen.complain("invalid");
+	karen.complain("debug");
+	karen.complain("info");
+	karen.complain("warning");
+	karen.complain("error");
+	
+	return (0);
+}
