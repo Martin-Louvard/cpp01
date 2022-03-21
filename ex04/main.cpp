69c1439ef43f300bbe2267b52d5f79c08f43124d
Martin <martin.louvard@gmail.com>
2022-03-21 14:47:03 +0100

    ex4

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,29 +1,41 @@
 #include <iostream>
 #include <fstream>
-# include <string>
+#include <string>
 
 int main(int argc, char **argv)
 {
     if (argc != 4)
         return (1);
+ 
+    std::string filename = argv[1];
+    std::ifstream ifs (filename);
     
-    std::ifstream ifs (argv[1]);
     if (!ifs.is_open())
         return (1);
-
+    
+    std::string s1(argv[2]);
+    std::string s2(argv[3]);
     std::string mytext;
     std::string line;
+    
     while ( std::getline (ifs,line) )
     {
       mytext.append(line).append("\n");
     }
-    std::cout << mytext;
-    ifs.close();
-    std::string s1(argv[2]);
-    std::string s2(argv[3]);
-    std::size_t found = 0;
-    
-
-
+    mytext = mytext.substr(0, mytext.size()-1);
+    size_t  pos(0);
+    size_t  prev_pos(pos);
+    std::string		repl_file = filename.append(".replace");
+	std::ofstream	ofs(repl_file);
+    while (true)
+    {
+        pos = mytext.find(s1, pos);
+        ofs << mytext.substr(prev_pos, pos - prev_pos);
+        if (pos == std::string::npos)
+            break;
+        ofs << s2;
+        pos += s1.size();
+        prev_pos = pos;
+    }
     return (0);
 }
